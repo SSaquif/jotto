465b3961ad21e06f66c2076103f59c9b758045e0
Sadnan Saquif <ssaquif@gmail.com>
2021-07-12 23:21:20 -0400

    Added some placeholder code to see app in practice, manual acceptance testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Congrats success={false} />
+      <GuessedWords
+        guessedWords={[
+          { guessedWord: "train", matchedLetterCount: 3 },
+          { guessedWord: "plane", matchedLetterCount: 1 },
+          { guessedWord: "crane", matchedLetterCount: 5 },
+        ]}
+      />
+    </div>
+  );
 }
 
 export default App;
